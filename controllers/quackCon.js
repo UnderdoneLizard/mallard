8c12b6c5c3d9c285f7056cec6d1fe99388cf18f1
UnderdoneLizard <mitchelljmarrs@gmail.com>
2020-10-28 16:30:18 -0400

    user can make a quack

--- controllers/quackCon.js
+++ controllers/quackCon.js
@@ -3,7 +3,24 @@ const router = express.Router();
 
 const db = require("../models");
 
+//create quack
+// TODO render create quack page 
 
+router.post('/create', async (req,res) => {
+    try{
+
+        const quack = await db.Quack.create(req.body);
+        quack.user = req.session.currentUser.id;
+        quack.save()
+        res.redirect('/')
+
+    } catch(error) {
+        console.log(error);
+        res.send({ message: "Internal server error" });
+    }
+})
+
+//delete quack
 
 
 
