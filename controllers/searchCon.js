3ad9a4d643f749f31c7cb7dfcc835a514e3775b7
UnderdoneLizard <mitchelljmarrs@gmail.com>
2020-10-30 11:39:12 -0400

    created a search for users

--- controllers/searchCon.js
+++ controllers/searchCon.js
@@ -6,7 +6,24 @@ const db = require("../models");
 //search routes
 //TODO refactor this to recieve results from the search... i think, or I could re render the page, I think that's what will happen we will see 
 router.get('/', (req, res) => {
-    res.render('core/search');
+
+    res.render('core/search', {results: undefined});
+
+})
+
+router.post('/', async (req, res) => {
+    try { 
+        console.log(req.body)
+        const results = await db.User.find({ $text: { $search: req.body.search }});
+        console.log(results);
+        context = {
+            results: results,
+        };
+        res.render('core/search', context);
+
+    } catch(error) {
+
+    }
 })
 
 
